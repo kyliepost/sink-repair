b0e0714853100a062e1d3eb0fb2d9b53427fef25
kyliepost <kylie.postlethwaite00@gmail.com>
2021-08-13 11:31:41 -0500

    created a requests.js module and updated dataAccess.js

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,4 +1,27 @@
 const applicationState = {
-
+requests: []
 }
 
+const API = "http://localhost:8088"
+
+export const fetchRequests = () => {
+    return fetch(`${API}/requests`)
+        .then(response => response.json())
+        .then(
+            (serviceRequests) => {
+                // Store the external state in application state
+                applicationState.requests = serviceRequests
+            }
+        )
+}
+
+
+// fetch ("http://localhost:5000/")
+// .then ((response) => {
+//     return response.json();
+// })
+// .then ((requestsData) => {
+//     console.log(requestsData)
+
+// }
+// )
